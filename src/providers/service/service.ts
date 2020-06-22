b6092c7b1a14d9f828e7931b9635254f7aa8253b
dwiHy <handayanijung@gmail.com>
2020-06-22 08:42:55 +0700

    UasAppmobmulti

---
+++ src/providers/service/service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ServiceProvider {
+  API_KEY = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=a9190def866e4af19bf9cac5fc75beeb';
+
+  constructor(public httpClient: HttpClient) {
+    console.log('Hello ServiceProvider Provider');
+  }
+
+  getNews(){
+    return this.httpClient.get(this.API_KEY);
+  }
+
+}
+
+
+
